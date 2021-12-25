344ef4814fb074274c77246fbb3b89b84bc005f4
Shalini Kumari <shalinisharma1017@gmail.com>
2021-12-25 12:51:54 -0600

    added take user input function to ask top range from user

--- Python Mini Projects/number_guesser_game.py
+++ Python Mini Projects/number_guesser_game.py
@@ -1,30 +1,55 @@
 import random
 
-STEP = 50
+start = 0
 
-i = 0
 tries = 0
 
 print("welcome to the number guesser game")
 
 
-def play():
-    number = random.randint(i, i + STEP)
-    #print(number)
+def take_user_input():
+    top_of_range = input("Please type top of range: ")
 
-    user_guess = input(f"guess a number between {i} and {i + STEP} ")
-    if int(user_guess) == number:
+    if top_of_range.isdigit():
+        top_of_range = int(top_of_range)
+    elif int(top_of_range) <= 0:
+        print("Please type a number greater than 0 next time")
+        quit()
+    else:
+        print("Please type a number next time")
+        quit()
+
+    return top_of_range
+
+
+def play():
+    top_of_range = take_user_input()
+    random_number = random.randrange(top_of_range)
+    print(random_number)
+
+    user_guess = input(f"guess a number between {start} and {top_of_range} ")
+    if int(user_guess) == random_number:
         print("You guessed it right!")
     else:
         print("you guessed it wrong!")
     play_again = input("You want to play again? (y/n) ")
-    if play_again == "y":
+    if play_again.lower() == "y":
         play()
+    elif playing.lower() == "n":
+        print("BYE")
+    else:
+        print("Enter only 'y' or 'n' ")
+        quit()
 
 
 playing = input("You want to play?(y/n) ")
 if playing.lower() == "y":
     play()
+elif playing.lower() == "n":
+    print("BYE")
+else:
+    print("Enter only 'y' or 'n' ")
+    quit()
 
 
 
