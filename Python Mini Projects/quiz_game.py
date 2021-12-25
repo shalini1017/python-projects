4383bca2f4a2a34acce10b8d54853277b653c160
Shalini Kumari <shalinisharma1017@gmail.com>
2021-12-24 18:11:55 -0600

    added play again functionality to the quiz game

--- Python Mini Projects/quiz_game.py
+++ Python Mini Projects/quiz_game.py
@@ -9,13 +9,29 @@ questions = list(ques_answer_dict)
 print("You will earn 1 point for each correct answer and get 0 for wrong ones\n")
 print("Answer the following questions\n")
 
-points = 0
 
-for question in questions:
-    print(question)
-    user_answer = input("Answer: ")
-    if user_answer == ques_answer_dict[question]:
-        points = points + 1
+def play():
+    points = 0
+    for question in questions:
+        print(question)
+        user_answer = input("Answer: ")
+        if user_answer == ques_answer_dict[question]:
+            points = points + 1
+
+    print(f"you have got {points} points")
+    play_again()
 
 
-print(f"you have got {points} points")
+def play_again():
+    print("if you want to play again?")
+    user_input = input("y/n: ")
+    if user_input == "y":
+        play()
+    elif user_input == "n":
+        print("bye")
+    else:
+        print("invalid input")
+        play_again()
+
+
+play()
