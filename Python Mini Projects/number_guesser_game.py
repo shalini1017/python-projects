ca3687b03d0c271dbe4fd8320abf7624521c56ab
Shalini Kumari <shalinisharma1017@gmail.com>
2021-12-25 15:54:43 -0600

    added function ask_play_again and repaced if statements with try except

--- Python Mini Projects/number_guesser_game.py
+++ Python Mini Projects/number_guesser_game.py
@@ -2,21 +2,16 @@ import random
 
 start = 0
 
-tries = 0
-
 print("welcome to the number guesser game")
 
 
 def take_user_input():
-    top_of_range = input("Please type top of range: ")
+    top_of_range = 0
 
-    if top_of_range.isdigit():
-        top_of_range = int(top_of_range)
-    elif int(top_of_range) <= 0:
-        print("Please type a number greater than 0 next time")
-        quit()
-    else:
-        print("Please type a number next time")
+    try:
+        top_of_range = int(input("Please type top of range: "))
+    except ValueError:
+        print("Oops! That is not a valid number. Try again..")
         quit()
 
     return top_of_range
@@ -26,20 +21,30 @@ def play():
     top_of_range = take_user_input()
     random_number = random.randrange(top_of_range)
     print(random_number)
+    print(f"guess a number between {start} and {top_of_range}")
+    tries = 0
+    while tries < 5:
+        tries += 1
 
-    user_guess = input(f"guess a number between {start} and {top_of_range} ")
-    if int(user_guess) == random_number:
-        print("You guessed it right!")
-    else:
-        print("you guessed it wrong!")
+        user_guess = input("guess: ")
+        if int(user_guess) == random_number:
+            print(f"You guessed it right! you took {tries} tries")
+            break
+        else:
+            print(f"you guessed it wrong! you took {tries} tries")
+            continue
+    ask_play_again()
+
+
+def ask_play_again():
     play_again = input("You want to play again? (y/n) ")
     if play_again.lower() == "y":
         play()
-    elif playing.lower() == "n":
-        print("BYE")
+    elif play_again.lower() == "n":
+        print("BYE BYE")
     else:
-        print("Enter only 'y' or 'n' ")
-        quit()
+        print("Enter only 'y' or 'n'")
+        ask_play_again()
 
 
 playing = input("You want to play?(y/n) ")
